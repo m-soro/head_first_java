c6357a7079050acfeca0ce2085a3d3d2c75620aa
Mark Soro <napoleon_soro@rocketmail.com>
2023-02-11 14:42:11 -0500

    added conditional java codes up to page 17

---
+++ BottleSong/BottleSong.java
@@ -0,0 +1,29 @@
+public class BottleSong {
+  
+  public static void main(String[] args) {
+    int bottlesNum = 10;
+    String word = "bottles";
+    
+  
+    while (bottlesNum > 0) {  
+      
+      System.out.println(bottlesNum + " green " + word + " hanging on the wall");
+      System.out.println(bottlesNum + " green " + word + " hanging on the wall");
+      System.out.println("And if one green bottle should accidentally fall,");
+      bottlesNum = bottlesNum - 1;
+       
+      if (bottlesNum == 1) {
+        word = "bottle"; // singular bottle
+      }
+
+
+      if (bottlesNum > 0) {
+        System.out.println("There'll be " + bottlesNum + " green " + word + ", hanging on the wall");
+      } else {
+        System.out.println("There'll be no green bottles hanging on the wall");
+      }
+
+             
+    } // end of while loop   	
+  } //end of method
+} //end of class
