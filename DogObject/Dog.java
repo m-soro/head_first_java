a370618f193cc227bfe4e1d581661cab01265a83
Mark Soro <napoleon_soro@rocketmail.com>
2023-02-12 15:57:42 -0500

    added OO codes up to page 37

---
+++ DogObject/Dog.java
@@ -0,0 +1,9 @@
+class Dog {
+    String name;
+    String breed;
+    float weight;
+
+    void greet() {
+        System.out.println("Ruff! Ruff! my name is " + name + " and I am a " + breed + ", I weigh " + weight);
+    }
+}
---
+++ DogObject/DogTestDrive.java
@@ -0,0 +1,15 @@
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog Gabby = new Dog();
+        Gabby.name = "Gabby";
+        Gabby.breed = "Labrador";
+        Gabby.weight = 25;
+        Dog Everson = new Dog();
+        Everson.name = "Everson";
+        Everson.breed = "Mastif";
+        Everson.weight = 40;
+        Everson.greet(); 
+       
+    
+    }
+}
